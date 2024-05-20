fc2323a359d3153fc6ba4b6015390c166b8d3b2b
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-05-20 17:55:31 +0700

    Add table's names and DbSet

--- PickleBall.Persistence/ApplicationDbContext.cs
+++ PickleBall.Persistence/ApplicationDbContext.cs
@@ -15,4 +15,32 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser, Applicati
         builder.ApplyConfigurationsFromAssembly(AssemblyReference.Assembly);
 
     public virtual DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+    public virtual DbSet<Ward> Wards { get; set; }
+
+    public virtual DbSet<CourtGroup> CourtGroups { get; set; }
+
+    public virtual DbSet<CourtYard> CourtYards { get; set; }
+
+    public virtual DbSet<Wallet> Wallets { get; set; }
+
+    public virtual DbSet<Transaction> Transactions { get; set; }
+
+    public virtual DbSet<Deposit> Deposits { get; set; }
+
+    public virtual DbSet<District> Districts { get; set; }
+
+    public virtual DbSet<City> Cities { get; set; }
+
+    public virtual DbSet<Media> Media { get; set; }
+
+    public virtual DbSet<BookMark> BookMarks { get; set; }
+
+    public virtual DbSet<Cost> Costs { get; set; }
+
+    public virtual DbSet<Slot> Slots { get; set; }
+
+    public virtual DbSet<Booking> Bookings { get; set; }
+
+    public virtual DbSet<SlotBooking> SlotBookings { get; set; }
 }
--- PickleBall.Persistence/Constants/TableNames.cs
+++ PickleBall.Persistence/Constants/TableNames.cs
@@ -6,4 +6,18 @@ internal static class TableNames
 {
     internal const string ApplicationUserTable = nameof(ApplicationUser);
     internal const string ApplicationRoleTable = nameof(ApplicationRole);
+    internal const string CourtGroup = nameof(CourtGroup);
+    internal const string CourtYard = nameof(CourtYard);
+    internal const string Ward = nameof(Ward);
+    internal const string Wallet = nameof(Wallet);
+    internal const string Transaction = nameof(Transaction);
+    internal const string Deposit = nameof(Deposit);
+    internal const string District = nameof(District);
+    internal const string City = nameof(City);
+    internal const string Media = nameof(Media);
+    internal const string BookMark = nameof(BookMark);
+    internal const string Cost = nameof(Cost);
+    internal const string Slot = nameof(Slot);
+    internal const string Booking = nameof(Booking);
+    internal const string SlotBooking = nameof(SlotBooking);
 }
