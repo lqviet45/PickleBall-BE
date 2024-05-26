ae8f06bda50a47125d32253be7cf0a2d20de6284
Long Bao <mglongbao@gmail.com>
2024-05-26 21:55:50 +0700

    fix: remove unnecessary file

--- PickleBall.Application/UseCases/Common/ICommandHandler.cs
+++
@@ -1,6 +0,0 @@
-namespace PickleBall.Application.UseCases.Common;
-
-public interface ICommandHandler<in TCommand, TCommandResult>
-{
-    Task<TCommandResult> Handle(TCommand command, CancellationToken cancellation);
-}
--- PickleBall.Application/UseCases/Common/IQueryDispatcher.cs
+++
@@ -1,6 +0,0 @@
-namespace PickleBall.Application.UseCases.Common;
-
-public interface IQueryDispatcher
-{
-    Task<TQueryResult> Dispatch<TQuery, TQueryResult>(TQuery query, CancellationToken cancellation);
-}
