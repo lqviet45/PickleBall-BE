fb974c281f9f760bfa51201df4efea4ff569a69e
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-05-28 17:01:09 +0700

    fix variable a to ID

--- PickleBall.Persistence/FakeDataGenerator/CityGenerator.cs
+++ PickleBall.Persistence/FakeDataGenerator/CityGenerator.cs
@@ -7,12 +7,12 @@ public static class CityGenerator
 {
     public static City[] InitializeDataForCities()
     {
-        int a = 1;
+        int ID = 1;
 
         return new Faker<City>()
             .UseSeed(2)
             .UseDateTimeReference(new DateTime(2021, 1, 1))
-            .RuleFor(city => city.Id, f => a++)
+            .RuleFor(city => city.Id, f => ID++)
             .RuleFor(city => city.Name, f => f.Address.City())
             .RuleFor(city => city.CreatedOnUtc, f => f.Date.Past())
             .RuleFor(city => city.ModifiedOnUtc, f => f.Date.Past())
