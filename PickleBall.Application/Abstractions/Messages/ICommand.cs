0753a55a6ca70e4ae12509dc7a4d06cdcccecb62
Long Bao <mglongbao@gmail.com>
2024-06-11 21:10:55 +0700

    Remove ICommand

--- PickleBall.Application/Abstractions/Messages/ICommand.cs
+++
@@ -1,8 +0,0 @@
-using Ardalis.Result;
-using MediatR;
-
-namespace PickleBall.Application.Abstractions.Messages;
-
-public interface ICommand : IRequest<Result> { }
-
-public interface ICommand<TResponse> : IRequest<Result<TResponse>> { }
