eca248380505cb2a406043516c00d4b6da730f32
Long Bao <mglongbao@gmail.com>
2024-06-20 18:34:32 +0700

    remove unused

--- PickleBall.API/Endpoints/CourtYards/GetAllByCourtGroupId/GetAllByCourtGroupIdEndpoint.cs
+++ PickleBall.API/Endpoints/CourtYards/GetAllByCourtGroupId/GetAllByCourtGroupIdEndpoint.cs
@@ -2,7 +2,6 @@ using Ardalis.ApiEndpoints;
 using Ardalis.Result;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using PickleBall.API.Endpoints.CourtGroups.GetCourtGroupById;
 using PickleBall.Application.UseCases.UseCase_CourtGroup.Queries.GetCourtGroupById;
 using PickleBall.Application.UseCases.UseCase_CourtYard.Queries.GetAllByCourtGroupId;
 using PickleBall.Domain.DTOs;
