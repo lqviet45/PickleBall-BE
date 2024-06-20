eb9c182fd4c1b0fe98ca982f32fa83c5fac5566e
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-20 18:23:21 +0700

    add mapping profile for slot

--- PickleBall.Application/Mapper/MappingProfile.cs
+++ PickleBall.Application/Mapper/MappingProfile.cs
@@ -14,5 +14,6 @@ public class MappingProfile : Profile
         CreateMap<CourtGroup, CourtGroupDto>().ReverseMap();
         CreateMap<CourtYard, CourtYardDto>().ReverseMap();
         CreateMap<District, DistrictDto>().ReverseMap();
+        CreateMap<Slot, SlotDto>().ReverseMap();
     }
 }
