7fa378faca09949ba8397f3b2a20f37ffafa618d
Long Bao <mglongbao@gmail.com>
2024-06-20 20:04:22 +0700

    Fix: Get correct user role

--- PickleBall.Application/UseCases/UseCase_ApplicationUser/Queries/GetUserByFirebaseId/GetUserByFirebaseIdHandler.cs
+++ PickleBall.Application/UseCases/UseCase_ApplicationUser/Queries/GetUserByFirebaseId/GetUserByFirebaseIdHandler.cs
@@ -1,17 +1,23 @@
 using Ardalis.Result;
 using AutoMapper;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 using PickleBall.Application.Abstractions;
 using PickleBall.Domain.DTOs;
+using PickleBall.Domain.DTOs.Enum;
 using PickleBall.Domain.Entities;
 
 namespace PickleBall.Application.UseCases.UseCase_ApplicationUser.Queries.GetUserByFirebaseId;
 
-internal sealed class GetUserByFirebaseIdHandler(IUnitOfWork unitOfWork, IMapper mapper)
-    : IRequestHandler<GetUserByFirebaseIdQuery, Result<ApplicationUserDto>>
+internal sealed class GetUserByFirebaseIdHandler(
+    IUnitOfWork unitOfWork,
+    IMapper mapper,
+    UserManager<ApplicationUser> userManager
+) : IRequestHandler<GetUserByFirebaseIdQuery, Result<ApplicationUserDto>>
 {
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
     private readonly IMapper _mapper = mapper;
+    private readonly UserManager<ApplicationUser> _userManager = userManager;
 
     public async Task<Result<ApplicationUserDto>> Handle(
         GetUserByFirebaseIdQuery request,
@@ -28,6 +34,7 @@ internal sealed class GetUserByFirebaseIdHandler(IUnitOfWork unitOfWork, IMapper
             return Result.NotFound("User is not found");
 
         ApplicationUserDto userDto = _mapper.Map<ApplicationUserDto>(user);
+        userDto.Role = (Role)Enum.Parse(typeof(Role), (await _userManager.GetRolesAsync(user))[0]);
 
         return Result.Success(userDto, "User is found successfully");
     }
