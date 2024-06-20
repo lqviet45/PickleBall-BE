701e7706a9ef4c6c73df6beaa6caf786d2e3352a
Long Bao <mglongbao@gmail.com>
2024-06-20 21:23:35 +0700

    Disable data generator

--- PickleBall.Persistence/ApplicationDbContext.cs
+++ PickleBall.Persistence/ApplicationDbContext.cs
@@ -17,60 +17,60 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser, Applicati
         base.OnModelCreating(builder);
         builder.ApplyConfigurationsFromAssembly(AssemblyReference.Assembly);
 
-        ApplicationUser[] users = ApplicationUserGenerator.InitializeDataForApplicationUsers();
-        City[] cities = CityGenerator.InitializeDataForCities();
-        District[] districts = DistrictGenerator.InitializeDataForDistricts(cities);
-        Wallet[] wallets = WalletGenerator.InitializeDataForWallets(users);
-        Deposit[] deposits = DepositGenerator.InitializeDataForDeposits(users, wallets);
-        Transaction[] transactions = TransactionGenerator.InitializeDataForTransactions(
-            users,
-            wallets,
-            deposits
-        );
-        Ward[] wards = WardGenerator.InitializeDataForWards(districts);
-        CourtGroup[] courtGroups = CourtGroupGenerator.InitializeDataForCourtGroups(
-            users,
-            wards,
-            wallets
-        );
-        Media[] media = MediaGenerator.InitializeDataForMedia(users, courtGroups);
-        CourtYard[] courtYards = CourtYardGenerator.InitializeDataForCourtYards(courtGroups);
-        Cost[] costs = CostGenerator.InitializeDataForCosts(courtYards);
-        Slot[] slots = SlotGenerator.InitializeDataForSlots(courtYards);
-        Date[] dates = DateGenerator.InitializeDataForDates();
-        DateCourtGroup[] dateCourtGroups = DateCourtGroupGenerator.InitializeDataForDateCourtGroup(
-            courtGroups,
-            dates
-        );
-        Booking[] bookings = BookingGenerator.InitializeDataForBookings(
-            users,
-            courtYards,
-            courtGroups,
-            dates
-        );
-        BookMark[] bookMarks = BookmarkGenerator.InitializeDataForBookMarks(users, courtGroups);
-        SlotBooking[] slotBookings = SlotBookingGenerator.InitializeDataForSlotBookings(
-            slots,
-            bookings
-        );
+        // ApplicationUser[] users = ApplicationUserGenerator.InitializeDataForApplicationUsers();
+        // City[] cities = CityGenerator.InitializeDataForCities();
+        // District[] districts = DistrictGenerator.InitializeDataForDistricts(cities);
+        // Wallet[] wallets = WalletGenerator.InitializeDataForWallets(users);
+        // Deposit[] deposits = DepositGenerator.InitializeDataForDeposits(users, wallets);
+        // Transaction[] transactions = TransactionGenerator.InitializeDataForTransactions(
+        //     users,
+        //     wallets,
+        //     deposits
+        // );
+        // Ward[] wards = WardGenerator.InitializeDataForWards(districts);
+        // CourtGroup[] courtGroups = CourtGroupGenerator.InitializeDataForCourtGroups(
+        //     users,
+        //     wards,
+        //     wallets
+        // );
+        // Media[] media = MediaGenerator.InitializeDataForMedia(users, courtGroups);
+        // CourtYard[] courtYards = CourtYardGenerator.InitializeDataForCourtYards(courtGroups);
+        // Cost[] costs = CostGenerator.InitializeDataForCosts(courtYards);
+        // Slot[] slots = SlotGenerator.InitializeDataForSlots(courtYards);
+        // Date[] dates = DateGenerator.InitializeDataForDates();
+        // DateCourtGroup[] dateCourtGroups = DateCourtGroupGenerator.InitializeDataForDateCourtGroup(
+        //     courtGroups,
+        //     dates
+        // );
+        // Booking[] bookings = BookingGenerator.InitializeDataForBookings(
+        //     users,
+        //     courtYards,
+        //     courtGroups,
+        //     dates
+        // );
+        // BookMark[] bookMarks = BookmarkGenerator.InitializeDataForBookMarks(users, courtGroups);
+        // SlotBooking[] slotBookings = SlotBookingGenerator.InitializeDataForSlotBookings(
+        //     slots,
+        //     bookings
+        // );
 
-        builder.Entity<ApplicationUser>().HasData(users);
-        builder.Entity<Booking>().HasData(bookings);
-        builder.Entity<BookMark>().HasData(bookMarks);
-        builder.Entity<City>().HasData(cities);
-        builder.Entity<Cost>().HasData(costs);
-        builder.Entity<CourtGroup>().HasData(courtGroups);
-        builder.Entity<CourtYard>().HasData(courtYards);
-        builder.Entity<Date>().HasData(dates);
-        builder.Entity<DateCourtGroup>().HasData(dateCourtGroups);
-        builder.Entity<Deposit>().HasData(deposits);
-        builder.Entity<District>().HasData(districts);
-        builder.Entity<Media>().HasData(media);
-        builder.Entity<Slot>().HasData(slots);
-        builder.Entity<SlotBooking>().HasData(slotBookings);
-        builder.Entity<Transaction>().HasData(transactions);
-        builder.Entity<Wallet>().HasData(wallets);
-        builder.Entity<Ward>().HasData(wards);
+        // builder.Entity<ApplicationUser>().HasData(users);
+        // builder.Entity<Booking>().HasData(bookings);
+        // builder.Entity<BookMark>().HasData(bookMarks);
+        // builder.Entity<City>().HasData(cities);
+        // builder.Entity<Cost>().HasData(costs);
+        // builder.Entity<CourtGroup>().HasData(courtGroups);
+        // builder.Entity<CourtYard>().HasData(courtYards);
+        // builder.Entity<Date>().HasData(dates);
+        // builder.Entity<DateCourtGroup>().HasData(dateCourtGroups);
+        // builder.Entity<Deposit>().HasData(deposits);
+        // builder.Entity<District>().HasData(districts);
+        // builder.Entity<Media>().HasData(media);
+        // builder.Entity<Slot>().HasData(slots);
+        // builder.Entity<SlotBooking>().HasData(slotBookings);
+        // builder.Entity<Transaction>().HasData(transactions);
+        // builder.Entity<Wallet>().HasData(wallets);
+        // builder.Entity<Ward>().HasData(wards);
     }
 
     public virtual DbSet<ApplicationUser> ApplicationUsers { get; set; }
