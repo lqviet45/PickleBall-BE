53614c257821b38cdfd1680426b9c1a17f83552b
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-21 21:30:34 +0700

    add interface and implement repo wallet and ward

---
+++ PickleBall.Persistence/Data/Repositories/RepositoryWallet.cs
@@ -0,0 +1,18 @@
+﻿using PickleBall.Contract.Abstractions.Repositories;
+using PickleBall.Domain.Entities;
+
+namespace PickleBall.Persistence.Data.Repositories
+{
+    internal sealed class RepositoryWallet : RepositoryBase<Wallet>, IRepositoryWallet
+    {
+        public RepositoryWallet(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public Task<Wallet?> GetWalletByIdAsync(
+            Guid walletId,
+            bool trackChanges,
+            CancellationToken cancellationToken = default)
+        => GetEntityByConditionAsync(w => w.Id == walletId, trackChanges, cancellationToken);
+    }
+}
