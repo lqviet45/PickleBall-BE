5df8d616a17b13de3ab8ac776aae5466c7a10183
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-21 21:29:01 +0700

    add repo wallet and ward

--- PickleBall.Persistence/Data/UnitOfWork.cs
+++ PickleBall.Persistence/Data/UnitOfWork.cs
@@ -19,6 +19,8 @@ namespace PickleBall.Persistence.Data
         private readonly Lazy<IRepositoryDate> _repositoryDate;
         private readonly Lazy<IRepositoryDistrict> _repositoryDistrict;
         private readonly Lazy<IRepositorySlot> _repositorySlot;
+        private readonly Lazy<IRepositoryWallet> _repositoryWallet;
+        private readonly Lazy<IRepositoryWard> _repositoryWard;
 
         public UnitOfWork(ApplicationDbContext context)
         {
@@ -41,6 +43,8 @@ namespace PickleBall.Persistence.Data
                 () => new RepositoryDistrict(context)
             );
             _repositorySlot = new Lazy<IRepositorySlot>(() => new RepositorySlot(context));
+            _repositoryWallet = new Lazy<IRepositoryWallet>(() => new RepositoryWallet(context));
+            _repositoryWard = new Lazy<IRepositoryWard>(() => new RepositoryWard(context));
         }
 
         public IRepositoryApplicationUser RepositoryApplicationUser =>
@@ -59,6 +63,10 @@ namespace PickleBall.Persistence.Data
         public IRepositoryDistrict RepositoryDistrict => _repositoryDistrict.Value;
 
         public IRepositorySlot RepositorySlot => _repositorySlot.Value;
+
+        public IRepositoryWallet RepositoryWallet => _repositoryWallet.Value;
+
+        public IRepositoryWard RepositoryWard => _repositoryWard.Value;
 
         public async Task SaveChangesAsync(CancellationToken cancellationToken)
         {
