addb58c5dbedfb345211792d1497438f838b247a
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-21 21:32:14 +0700

    add create courtgroup by ownerid

---
+++ PickleBall.API/Endpoints/CourtGroups/CreateCourtGroupByOwnerId/CreateCourtGroupByOwnerId.cs
@@ -0,0 +1,81 @@
+﻿using Ardalis.Result;
+using PickleBall.Application.UseCases.UseCase_ApplicationUser.Queries.GetUserById;
+using PickleBall.Domain.DTOs.Enum;
+using PickleBall.Domain.DTOs;
+using MediatR;
+using PickleBall.Application.UseCases.UseCase_CourtGroup.Commands.CreateCourtGroup;
+using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PickleBall.API.Endpoints.CourtGroups.CreateCourtGroupByOwnerId
+{
+    public record CreateCourtGroupRequest
+    {
+        [FromRoute]
+        public Guid UserId { get; set; }
+        [FromQuery]
+        public string WardName { get; set; }
+        [FromQuery]
+        public Guid WalletId { get; set; }
+        [FromQuery]
+        public string? Name { get; set; }
+        [FromQuery]
+        public decimal Price { get; set; }
+        [FromQuery]
+        public int MinSlots { get; set; }
+        [FromQuery]
+        public int MaxSlots { get; set; }
+    }
+
+    public class CreateCourtGroupByOwnerId : EndpointBaseAsync.WithRequest<CreateCourtGroupRequest>.WithActionResult<CourtGroupDto>
+    {
+        private readonly IMediator _mediator;
+
+        public CreateCourtGroupByOwnerId(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [Route("/api/users/{UserId}/court-group")]
+        public override async Task<ActionResult<CourtGroupDto>> HandleAsync(CreateCourtGroupRequest request, CancellationToken cancellationToken = default)
+        {
+            var user = await GetUserAndCheckRoleAsync(request.UserId, cancellationToken);
+
+            if (!user.IsSuccess)
+                return user.IsNotFound() ? NotFound(user) : Unauthorized(user);
+
+            Result<CourtGroupDto> result = await _mediator.Send(
+                new CreateCourtGroupCommand
+                {
+                    UserId = request.UserId,
+                    WardName = request.WardName,
+                    WalletId = request.WalletId,
+                    Name = request.Name,
+                    Price = request.Price,
+                    MinSlots = request.MinSlots,
+                }, cancellationToken);
+
+            if (!result.IsSuccess)
+                return BadRequest(result);
+
+            return Created("Created successfully!", result);
+        }
+
+        private async Task<Result<ApplicationUserDto>> GetUserAndCheckRoleAsync(
+        Guid id,
+        CancellationToken cancellationToken = default
+    )
+        {
+            var user = await _mediator.Send(new GetUserByIdQuery { Id = id }, cancellationToken);
+
+            if (!user.IsSuccess)
+                return Result.NotFound("User is not found");
+
+            if (user.Value.Role != Role.Owner)
+                return Result.Unauthorized();
+
+            return user;
+        }
+    }
+}
