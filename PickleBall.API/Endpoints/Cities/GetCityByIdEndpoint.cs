c0e7455cc9556d32697be6a141adcb9f3f6980d3
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-22 17:52:42 +0700

    add get city by id endpoint

---
+++ PickleBall.API/Endpoints/Cities/GetCityByIdEndpoint.cs
@@ -0,0 +1,37 @@
+﻿using Ardalis.ApiEndpoints;
+using Ardalis.Result;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PickleBall.Application.UseCases.UseCase_City.Queries.GetCityById;
+using PickleBall.Domain.DTOs;
+
+namespace PickleBall.API.Endpoints.Cities
+{
+    public record GetCityByIdRequest
+    {
+        [FromRoute]
+        public int Id { get; set; }
+    }
+
+    public class GetCityByIdEndpoint : EndpointBaseAsync.WithRequest<GetCityByIdRequest>.WithActionResult<Result<CityDto>>
+    {
+        private readonly IMediator _mediator;
+
+        public GetCityByIdEndpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("/api/cities/{Id}")]
+        public override async Task<ActionResult<Result<CityDto>>> HandleAsync(GetCityByIdRequest request, CancellationToken cancellationToken = default)
+        {
+            Result<CityDto> result = await _mediator.Send(new GetCityByIdQuery { Id = request.Id }, cancellationToken);
+
+            if (!result.IsSuccess)
+                return result.IsNotFound() ? NotFound(result) : BadRequest(result);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ PickleBall.Application/UseCases/UseCase_City/Queries/GetCityById/GetCityByIdHandler.cs
@@ -0,0 +1,34 @@
+﻿using Ardalis.Result;
+using AutoMapper;
+using MediatR;
+using PickleBall.Application.Abstractions;
+using PickleBall.Domain.DTOs;
+
+namespace PickleBall.Application.UseCases.UseCase_City.Queries.GetCityById
+{
+    internal sealed class GetCityByIdHandler : IRequestHandler<GetCityByIdQuery, Result<CityDto>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetCityByIdHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<CityDto>> Handle(GetCityByIdQuery request, CancellationToken cancellationToken)
+        {
+            var city = await _unitOfWork.RepositoryCity.GetEntityByConditionAsync(
+                               c => c.Id == request.Id,
+                               request.trackChanges,
+                               cancellationToken);
+            if (city is null)
+                return Result.NotFound("City is not found");
+
+            var cityDto = _mapper.Map<CityDto>(city);
+
+            return Result.Success(cityDto, "City is found successfully");
+        }
+    }
+}
