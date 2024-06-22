d9a0d3c042d1786db55433ecd8f460d2b95849a2
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-22 10:38:32 +0700

    add get booking by id endpoint

---
+++ PickleBall.Application/UseCases/UseCase_Booking/Queries/GetBookingById/GetBookingByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Result;
+using MediatR;
+using PickleBall.Domain.DTOs;
+
+namespace PickleBall.Application.UseCases.UseCase_Booking.Queries.GetBookingById
+{
+    public class GetBookingByIdQuery : IRequest<Result<BookingDto>>
+    {
+        public Guid BookingId { get; set; }
+        public bool TrackChanges { get; set; } = false;
+    }
+}
