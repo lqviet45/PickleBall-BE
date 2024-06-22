296c8aee2299ebebf9a7c2a22fc8af2242c11ff6
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-22 10:08:40 +0700

    add MediaDto and create map

---
+++ PickleBall.Domain/DTOs/MediaDto.cs
@@ -0,0 +1,12 @@
+﻿namespace PickleBall.Domain.DTOs
+{
+    public class MediaDto
+    {
+        public Guid Id { get; set; }
+        public string? MediaUrl { get; set; }
+        public Guid? UserId { get; set; }
+        public Guid? CourtGroupId { get; set; }
+        public DateTimeOffset CreatedOnUtc { get; set; }
+        public DateTimeOffset? ModifiedOnUtc { get; set; }
+    }
+}
