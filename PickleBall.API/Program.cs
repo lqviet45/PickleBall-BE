e584ed21f6ad21ed89f20a8cd8b14107d752528b
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-22 16:01:14 +0700

    add nuget for auto convert enum to string

--- PickleBall.API/Program.cs
+++ PickleBall.API/Program.cs
@@ -13,7 +13,11 @@ builder.Services.AddFireBase();
 builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.Converters.Add(new StringEnumConverter());
+    });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
