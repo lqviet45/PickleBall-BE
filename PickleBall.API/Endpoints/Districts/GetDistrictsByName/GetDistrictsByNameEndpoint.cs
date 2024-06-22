c618a893ab227d9d72dc178bc269069b6a68964b
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-22 08:38:01 +0700

    add get districts by name (search)

---
+++ PickleBall.API/Endpoints/Districts/GetDistrictsByName/GetDistrictsByNameEndpoint.cs
@@ -0,0 +1,42 @@
+﻿using Ardalis.ApiEndpoints;
+using Ardalis.Result;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PickleBall.Application.UseCases.UseCase_District.Queries.GetDistrictByName;
+using PickleBall.Domain.DTOs;
+
+namespace PickleBall.API.Endpoints.Districts.GetDistrictsByName
+{
+    public record GetDistrictsByNameRequest
+    {
+        [FromQuery]
+        public string Name { get; set; } = string.Empty;
+    }
+
+    public class GetDistrictsByNameEndpoint : EndpointBaseAsync.WithRequest<GetDistrictsByNameRequest>.WithActionResult<IEnumerable<DistrictDto>>
+    {
+        private readonly IMediator _mediator;
+
+        public GetDistrictsByNameEndpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("/api/districts/search")]
+        public override async Task<ActionResult<IEnumerable<DistrictDto>>> HandleAsync(GetDistrictsByNameRequest request, CancellationToken cancellationToken = default)
+        {
+            Result<IEnumerable<DistrictDto>> result = await _mediator.Send(
+                                              new GetDistrictsByNameQuery
+                                              {
+                                                  Name = request.Name,
+                                              },
+                                              cancellationToken);
+
+            if (!result.IsSuccess)
+                return result.IsNotFound() ? NotFound(result) : BadRequest(result);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ PickleBall.Application/UseCases/UseCase_District/Queries/GetDistrictByName/GetDistrictsByNameHandler.cs
@@ -0,0 +1,35 @@
+﻿using Ardalis.Result;
+using AutoMapper;
+using MediatR;
+using PickleBall.Application.Abstractions;
+using PickleBall.Domain.DTOs;
+
+namespace PickleBall.Application.UseCases.UseCase_District.Queries.GetDistrictByName
+{
+    internal sealed class GetDistrictsByNameHandler : IRequestHandler<GetDistrictsByNameQuery, Result<IEnumerable<DistrictDto>>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetDistrictsByNameHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<IEnumerable<DistrictDto>>> Handle(GetDistrictsByNameQuery request, CancellationToken cancellationToken)
+        {
+            var districts = await _unitOfWork.RepositoryDistrict.GetEntitiesByConditionAsync(
+                               district => district.Name != null && district.Name.Contains(request.Name),
+                               request.TrackChanges,
+                               cancellationToken);
+
+            if (!districts.Any())
+                return Result.NotFound("Districts are not found");
+
+            var districtDtos = _mapper.Map<IEnumerable<DistrictDto>>(districts);
+
+            return Result.Success(districtDtos, "Districts are found successfully!");
+        }
+    }
+}
