b92f041832c7bbe97252c022710fd81ccc2b404f
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-22 18:36:07 +0700

    add endpoint get media by id endpoint

---
+++ PickleBall.API/Endpoints/Medias/GetMediaByIdEndpoint.cs
@@ -0,0 +1,37 @@
+﻿using Ardalis.ApiEndpoints;
+using Ardalis.Result;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PickleBall.Application.UseCases.UseCase_Media.Queries.GetMediaById;
+using PickleBall.Domain.DTOs;
+
+namespace PickleBall.API.Endpoints.Medias
+{
+    public record GetMediaByIdRequest
+    {
+        [FromRoute]
+        public Guid Id { get; init; }
+    }
+
+    public class GetMediaByIdEndpoint : EndpointBaseAsync.WithRequest<GetMediaByIdRequest>.WithActionResult<Result<MediaDto>>
+    {
+        private readonly IMediator _mediator;
+
+        public GetMediaByIdEndpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("/api/medias/{Id}")]
+        public override async Task<ActionResult<Result<MediaDto>>> HandleAsync(GetMediaByIdRequest request, CancellationToken cancellationToken = default)
+        {
+            Result<MediaDto> result = await _mediator.Send(new GetMediaByIdQuery { Id = request.Id }, cancellationToken);
+
+            if (!result.IsSuccess)
+                return result.IsNotFound() ? NotFound(result) : BadRequest(result);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ PickleBall.Application/UseCases/UseCase_Media/Queries/GetMediaById/GetMediaByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Result;
+using MediatR;
+using PickleBall.Domain.DTOs;
+
+namespace PickleBall.Application.UseCases.UseCase_Media.Queries.GetMediaById
+{
+    public class GetMediaByIdQuery : IRequest<Result<MediaDto>>
+    {
+        public Guid Id { get; set; }
+        public bool trackChanges { get; set; } = false;
+    }
+}
