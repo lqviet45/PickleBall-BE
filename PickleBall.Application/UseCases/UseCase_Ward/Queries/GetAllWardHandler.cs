5fa2a75d1363ef9c7880f2c52ead26e78867d0a5
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-22 13:19:25 +0700

    add get all wards endpoint

---
+++ PickleBall.Application/UseCases/UseCase_Ward/Queries/GetAllWardHandler.cs
@@ -0,0 +1,32 @@
+﻿using Ardalis.Result;
+using AutoMapper;
+using MediatR;
+using PickleBall.Application.Abstractions;
+using PickleBall.Domain.DTOs;
+
+namespace PickleBall.Application.UseCases.UseCase_Ward.Queries
+{
+    internal sealed class GetAllWardHandler : IRequestHandler<GetAllWardQuery, Result<IEnumerable<WardDto>>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetAllWardHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<IEnumerable<WardDto>>> Handle(GetAllWardQuery request, CancellationToken cancellationToken)
+        {
+            var wards = await _unitOfWork.RepositoryWard.GetAllAsync(request.TrackChanges, cancellationToken);
+
+            if (!wards.Any())
+                return Result.NotFound("Wards are not found");
+
+            var wardDtos = _mapper.Map<IEnumerable<WardDto>>(wards);
+
+            return Result.Success(wardDtos, "Wards are found successfully");
+        }
+    }
+}
---
+++ PickleBall.Application/UseCases/UseCase_Ward/Queries/GetAllWardQuery.cs
@@ -0,0 +1,11 @@
+﻿using Ardalis.Result;
+using MediatR;
+using PickleBall.Domain.DTOs;
+
+namespace PickleBall.Application.UseCases.UseCase_Ward.Queries
+{
+    public class GetAllWardQuery : IRequest<Result<IEnumerable<WardDto>>>
+    {
+        public bool TrackChanges { get; set; } = false;
+    }
+}
