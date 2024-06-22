37178532a4b200ed8b658b4214db72e9ff86081b
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-22 10:12:44 +0700

    add get medias by userid endpoint

---
+++ PickleBall.Persistence/Data/Repositories/RepositoryMedia.cs
@@ -0,0 +1,19 @@
+﻿using PickleBall.Contract.Abstractions.Repositories;
+using PickleBall.Domain.Entities;
+
+namespace PickleBall.Persistence.Data.Repositories
+{
+    internal sealed class RepositoryMedia : RepositoryBase<Media>, IRepositoryMedia
+    {
+        public RepositoryMedia(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Media>> GetMediasByUserId(Guid UserId, bool trackChanges, CancellationToken cancellationToken = default)
+        => await GetEntitiesByConditionAsync(
+               media => media.UserId == UserId && media.MediaUrl != null,
+               trackChanges,
+               cancellationToken
+           );
+    }
+}
