666cc4f96487ce2ceabd3ec1e5b4fecbe50008bd
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-22 14:32:20 +0700

    add get ward by name endpoint (search)

---
+++ PickleBall.Application/UseCases/UseCase_Ward/Queries/GetWardsByName/GetWardsByNameHandler.cs
@@ -0,0 +1,39 @@
+﻿using Ardalis.Result;
+using AutoMapper;
+using MediatR;
+using PickleBall.Application.Abstractions;
+using PickleBall.Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickleBall.Application.UseCases.UseCase_Ward.Queries.GetWardsByName
+{
+    internal sealed class GetWardsByNameHandler : IRequestHandler<GetWardsByNameQuery, Result<IEnumerable<WardDto>>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetWardsByNameHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<IEnumerable<WardDto>>> Handle(GetWardsByNameQuery request, CancellationToken cancellationToken)
+        {
+            var wards = await _unitOfWork.RepositoryWard.GetEntitiesByConditionAsync(
+                                           ward => ward.Name != null && ward.Name.Contains(request.Name),
+                                           request.TrackChanges,
+                                           cancellationToken);
+
+            if (!wards.Any())
+                return Result.NotFound("Wards are not found");
+
+            var wardDtos = _mapper.Map<IEnumerable<WardDto>>(wards);
+            return Result.Success(wardDtos, "Wards are found successfully!");
+        }
+    }
+}
