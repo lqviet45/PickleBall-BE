646d96c4d217b1123f237f69d02b1a27a781a8e4
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-22 19:53:32 +0700

    add endpoint get courtyards by name (search)

---
+++ PickleBall.Application/UseCases/UseCase_CourtYard/Queries/GetCourtYardsByName/GetCourtYardsByNameQuery.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Result;
+using MediatR;
+using PickleBall.Domain.DTOs;
+
+namespace PickleBall.Application.UseCases.UseCase_CourtYard.Queries.GetCourtYardsByName
+{
+    public class GetCourtYardsByNameQuery : IRequest<Result<IEnumerable<CourtYardDto>>>
+    {
+        public string Name { get; set; } = string.Empty;
+        public bool TrackChanges { get; set; } = false;
+    }
+}
