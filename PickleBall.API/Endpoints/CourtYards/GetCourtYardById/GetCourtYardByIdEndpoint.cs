5dbaebeae07403cdf4ed6feb9a7912a19819d5f0
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-22 16:48:21 +0700

    add get court yard by id

---
+++ PickleBall.API/Endpoints/CourtYards/GetCourtYardById/GetCourtYardByIdEndpoint.cs
@@ -0,0 +1,37 @@
+﻿using Ardalis.ApiEndpoints;
+using Ardalis.Result;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PickleBall.Application.UseCases.UseCase_CourtYard.Queries.GetCourtYardById;
+using PickleBall.Domain.DTOs;
+
+namespace PickleBall.API.Endpoints.CourtYards.GetCourtYardById
+{
+    public record GetCourtYardByIdRequest
+    {
+        [FromRoute]
+        public Guid Id { get; set; }
+    }
+
+    public class GetCourtYardByIdEndpoint : EndpointBaseAsync.WithRequest<GetCourtYardByIdRequest>.WithActionResult<Result<CourtYardDto>>
+    {
+        private readonly IMediator _mediator;
+
+        public GetCourtYardByIdEndpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("/api/court-yards/{Id}")]
+        public override async Task<ActionResult<Result<CourtYardDto>>> HandleAsync(GetCourtYardByIdRequest request, CancellationToken cancellationToken = default)
+        {
+            Result<CourtYardDto> result = await _mediator.Send(new GetCourtYardByIdQuery { CourtYardId = request.Id }, cancellationToken);
+
+            if (!result.IsSuccess)
+                return result.IsNotFound() ? NotFound(result) : BadRequest(result);
+
+            return Ok(result);
+        }
+    }
+}
