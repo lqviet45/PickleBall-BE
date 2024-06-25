4e9a4a72f0ffcae276e1a4bf25e4f7486efb80e5
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-25 11:43:39 +0700

    add new migration

---
+++ PickleBall.Persistence/Migrations/20240625041017_RemoveWalletFK.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PickleBall.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveWalletFK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourtGroup_Wallet_WalletId",
+                table: "CourtGroup");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CourtGroup_WalletId",
+                table: "CourtGroup");
+
+            migrationBuilder.DropColumn(
+                name: "WalletId",
+                table: "CourtGroup");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "WalletId",
+                table: "CourtGroup",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourtGroup_WalletId",
+                table: "CourtGroup",
+                column: "WalletId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourtGroup_Wallet_WalletId",
+                table: "CourtGroup",
+                column: "WalletId",
+                principalTable: "Wallet",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
