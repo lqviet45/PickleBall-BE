fc8262b392bbbeec876ecd07ae864e3827f2fbb2
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-28 22:27:37 +0700

    Feat: delete bookmark

---
+++ PickleBall.API/Endpoints/BookMarks/DeleteBookMarkEndpoint.cs
@@ -0,0 +1,46 @@
+﻿using Ardalis.ApiEndpoints;
+using Ardalis.Result;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PickleBall.Application.UseCases.UseCase_BookMark.Commands.DeleteBookMark;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace PickleBall.API.Endpoints.BookMarks
+{
+    public record DeleteBookMarkRequest
+    {
+        [FromRoute]
+        public Guid Id { get; set; }
+    }
+
+    public class DeleteBookMarkEndpoint : EndpointBaseAsync.WithRequest<DeleteBookMarkRequest>.WithActionResult
+    {
+        private readonly IMediator _mediator;
+
+        public DeleteBookMarkEndpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpDelete]
+        [Route("/api/bookmarks/{Id}")]
+        [SwaggerOperation(
+            Summary = "Delete a bookmark",
+            Description = "Delete a bookmark",
+            OperationId = "BookMarks.DeleteBookMark",
+            Tags = new[] { "BookMarks" }
+        )]
+        public override async Task<ActionResult> HandleAsync(DeleteBookMarkRequest request, CancellationToken cancellationToken = default)
+        {
+            var result = await _mediator.Send(new DeleteBookMarkCommand
+            {
+                Id = request.Id
+            });
+
+            if (!result.IsSuccess)
+                return result.IsNotFound() ? NotFound(result) : BadRequest(result);
+
+            return NoContent();
+        }
+    }
+}
