0a712dbea2bf63ce7116114f3785c0210de01681
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-28 21:22:16 +0700

    add create bookmark

---
+++ PickleBall.Application/UseCases/UseCase_BookMark/Commands/CreateBookMark/CreateBookMarkCommand.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Result;
+using MediatR;
+using PickleBall.Domain.DTOs;
+
+namespace PickleBall.Application.UseCases.UseCase_BookMark.Commands.CreateBookMark
+{
+    public class CreateBookMarkCommand : IRequest<Result<BookMarkDto>>
+    {
+        public Guid UserId { get; set; }
+        public Guid CourtId { get; set; }
+    }
+}
