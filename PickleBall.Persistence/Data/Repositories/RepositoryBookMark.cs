f27d0f90d597fb945f9a0cf2badc4371545e72ea
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-29 00:11:44 +0700

    add paging class for bookmark add paging function

--- PickleBall.Persistence/Data/Repositories/RepositoryBookMark.cs
+++ PickleBall.Persistence/Data/Repositories/RepositoryBookMark.cs
@@ -1,5 +1,8 @@
-﻿using PickleBall.Contract.Abstractions.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+using PickleBall.Contract.Abstractions.Repositories;
 using PickleBall.Domain.Entities;
+using PickleBall.Domain.Paging;
+using System.Linq.Expressions;
 
 namespace PickleBall.Persistence.Data.Repositories
 {
@@ -8,5 +11,46 @@ namespace PickleBall.Persistence.Data.Repositories
         public RepositoryBookMark(ApplicationDbContext context) : base(context)
         {
         }
+
+        public async Task<IEnumerable<BookMark>> GetAllBookMarksAsync(bool trackChanges, BookMarkParameters bookMarkParameters, CancellationToken cancellationToken = default)
+        =>
+            await GetAllAsync(
+            trackChanges,
+            cancellationToken,
+            query =>
+                query
+                    .Include(c => c.CourtGroup)
+                    .Include(c => c.User)
+                    .Skip((bookMarkParameters.PageNumber - 1) * bookMarkParameters.PageSize)
+                    .Take(bookMarkParameters.PageSize)
+            );
+
+        public async Task<IEnumerable<BookMark>> GetAllBookMarksByConditionAsync(Expression<Func<BookMark, bool>> conditions, bool trackChanges, BookMarkParameters bookMarkParameters, CancellationToken cancellationToken = default)
+        =>
+            await GetEntitiesByConditionAsync(
+            conditions,
+            trackChanges,
+            cancellationToken,
+            query =>
+                query
+                    .Include(c => c.CourtGroup)
+                    .Include(c => c.User)
+                    .Skip((bookMarkParameters.PageNumber - 1) * bookMarkParameters.PageSize)
+                    .Take(bookMarkParameters.PageSize)
+            );
+
+        public async Task<BookMark?> GetBookMarkByConditionAsync(Expression<Func<BookMark, bool>> conditions, bool trackChanges, BookMarkParameters bookMarkParameters, CancellationToken cancellationToken = default)
+        =>
+            await GetEntityByConditionAsync(
+            conditions,
+            trackChanges,
+            cancellationToken,
+            query =>
+                query
+                    .Include(c => c.CourtGroup)
+                    .Include(c => c.User)
+                    .Skip((bookMarkParameters.PageNumber - 1) * bookMarkParameters.PageSize)
+                    .Take(bookMarkParameters.PageSize)
+            );
     }
 }
