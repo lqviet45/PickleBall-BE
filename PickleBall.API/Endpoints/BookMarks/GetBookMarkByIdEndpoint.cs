22e64a1bebc6d8b17d687be2e37a32a772a28983
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-28 23:24:13 +0700

    Feat: get bookmark by id

---
+++ PickleBall.API/Endpoints/BookMarks/GetBookMarkByIdEndpoint.cs
@@ -0,0 +1,46 @@
+﻿using Ardalis.ApiEndpoints;
+using Ardalis.Result;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PickleBall.Application.UseCases.UseCase_BookMark.Queries.GetBookMarkById;
+using PickleBall.Domain.DTOs;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace PickleBall.API.Endpoints.BookMarks
+{
+    public record GetBookMarkByIdRequest
+    {
+        [FromRoute]
+        public Guid Id { get; set; }
+    }
+
+    public class GetBookMarkByIdEndpoint : EndpointBaseAsync.WithRequest<GetBookMarkByIdRequest>.WithActionResult<Result<BookMarkDto>>
+    {
+        private readonly IMediator _mediator;
+
+        public GetBookMarkByIdEndpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("/api/bookmarks/{Id}")]
+        [SwaggerOperation(
+            Summary = "Get bookmark by id",
+            Description = "Get bookmark by id",
+            OperationId = "BookMarks.GetBookMarkById",
+            Tags = new[] { "BookMarks" }
+        )]
+        public override async Task<ActionResult<Result<BookMarkDto>>> HandleAsync(GetBookMarkByIdRequest request, CancellationToken cancellationToken = default)
+        {
+            Result<BookMarkDto> result = await _mediator.Send(
+                new GetBookMarkByIdQuery { Id = request.Id },
+                cancellationToken);
+
+            if (!result.IsSuccess)
+                return result.IsNotFound() ? NotFound(result) : BadRequest(result);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ PickleBall.Application/UseCases/UseCase_BookMark/Queries/GetBookMarkById/GetBookMarkByIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Result;
+using MediatR;
+using PickleBall.Domain.DTOs;
+
+namespace PickleBall.Application.UseCases.UseCase_BookMark.Queries.GetBookMarkById
+{
+    public class GetBookMarkByIdQuery : IRequest<Result<BookMarkDto>>
+    {
+        public Guid Id { get; set; }
+
+        public bool TrackChanges { get; set; } = false;
+    }
+}
