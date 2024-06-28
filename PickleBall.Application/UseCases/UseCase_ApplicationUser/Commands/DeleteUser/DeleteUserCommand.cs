dc6be0675023b11c010854f3391774deb17f00c3
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-28 19:02:40 +0700

    Feat: delete user

---
+++ PickleBall.Application/UseCases/UseCase_ApplicationUser/Commands/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using Ardalis.Result;
+using MediatR;
+
+namespace PickleBall.Application.UseCases.UseCase_ApplicationUser.Commands.DeleteUser
+{
+    public class DeleteUserCommand : IRequest<Result>
+    {
+        public Guid Id { get; set; }
+    }
+}
