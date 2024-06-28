ebfeeffa97d3147e726c017ff8f96fe07ba38f43
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-29 00:11:05 +0700

    Feat: Get all bookmarks

---
+++ PickleBall.API/Endpoints/BookMarks/GetAllBookMarkEndpoint.cs
@@ -0,0 +1,57 @@
+﻿using Ardalis.ApiEndpoints;
+using Ardalis.Result;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PickleBall.Application.UseCases.UseCase_BookMark.Queries.GetAllBookMark;
+using PickleBall.Domain.DTOs;
+using PickleBall.Domain.Paging;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace PickleBall.API.Endpoints.BookMarks
+{
+    public record GetAllBookMarkRequest
+    {
+        [FromQuery]
+        public int PageNumber { get; set; } = 1;
+
+        [FromQuery]
+        public int PageSize { get; set; } = 10;
+    }
+
+    public class GetAllBookMarkEndpoint : EndpointBaseAsync.WithRequest<GetAllBookMarkRequest>.WithActionResult<Result<IEnumerable<BookMarkDto>>>
+    {
+        private readonly IMediator _mediator;
+
+        public GetAllBookMarkEndpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("/api/bookmarks")]
+        [SwaggerOperation(
+            Summary = "Get all bookmarks",
+            Description = "Get all bookmarks",
+            OperationId = "BookMarks.GetAllBookMark",
+            Tags = new[] { "BookMarks" }
+        )]
+        public override async Task<ActionResult<Result<IEnumerable<BookMarkDto>>>> HandleAsync(GetAllBookMarkRequest request, CancellationToken cancellationToken = default)
+        {
+            var bookMarkParameters = new BookMarkParameters
+            {
+                PageNumber = request.PageNumber,
+                PageSize = request.PageSize
+            };
+
+            Result<IEnumerable<BookMarkDto>> result = await _mediator.Send(new GetAllBookMarkQuery
+            {
+                BookMarkParameters = bookMarkParameters,
+            }, cancellationToken);
+
+            if (!result.IsSuccess)
+                return result.IsNotFound() ? NotFound(result) : BadRequest(result);
+
+            return Ok(result);
+        }
+    }
+}
