9f05447847d90ddcd8ab7d207cc78618df2da8a3
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-06-29 22:55:46 +0700

    Feat: Get bookmark by UserId And CourtGroupId

---
+++ PickleBall.Application/UseCases/UseCase_BookMark/Queries/GetBookMarkByUserIdAndCourtGroupId/GetBookMarkByUserIdAndCourtGroupIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Result;
+using MediatR;
+using PickleBall.Domain.DTOs;
+
+namespace PickleBall.Application.UseCases.UseCase_BookMark.Queries.GetBookMarkByUserIdAndCourtGroupId
+{
+    public class GetBookMarkByUserIdAndCourtGroupIdQuery : IRequest<Result<BookMarkDto>>
+    {
+        public Guid UserId { get; set; }
+        public Guid CourtGroupId { get; set; }
+    }
+}
