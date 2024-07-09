856cf6ac4d7299196dd8acd2bb746ea5abbf8f39
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-07-10 00:15:03 +0700

    add mapper and dto for slot booking add slot params for paging

---
+++ PickleBall.Domain/DTOs/SlotBookingDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickleBall.Domain.DTOs
+{
+    public class SlotBookingDto
+    {
+        public Guid Id { get; set; }
+        public Guid SlotId { get; set; }
+        public Guid BookingId { get; set; }
+        public DateTimeOffset CreatedOnUtc { get; set; }
+        public DateTimeOffset? ModifiedOnUtc { get; set; }
+        public DateTime BookingDate { get; set; }
+    }
+}
