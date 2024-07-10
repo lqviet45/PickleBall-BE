c0aed2b230b3b3b0f93c37a1ee8dae0b2f2f7f2f
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-07-10 10:39:51 +0700

    Feat: Get Bookings By CourtGroupId

---
+++ PickleBall.API/Endpoints/Bookings/GetBookingsByCourtGroupId/GetBookingsByCourtGroupIdEndpoint.cs
@@ -0,0 +1,63 @@
+﻿using Ardalis.ApiEndpoints;
+using Ardalis.Result;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PickleBall.Application.Abstractions;
+using PickleBall.Application.UseCases.UseCase_Booking.Queries.GetBookingByUserId;
+using PickleBall.Application.UseCases.UseCase_Booking.Queries.GetBookingsByCourtGroupId;
+using PickleBall.Domain.DTOs.BookingDtos;
+using PickleBall.Domain.Paging;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace PickleBall.API.Endpoints.Bookings.GetBookingsByCourtGroupId
+{
+    public record GetBookingsByCourtGroupIdRequest
+    {
+        [FromRoute]
+        public Guid CourtGroupId { get; init; }
+
+        [FromQuery]
+        public int PageNumber { get; init; } = 1;
+
+        [FromQuery]
+        public int PageSize { get; init; } = 10;
+    }
+
+    public class GetBookingsByCourtGroupIdEndpoint : EndpointBaseAsync.WithRequest<GetBookingsByCourtGroupIdRequest>.WithActionResult
+    {
+        private readonly IMediator _mediator;
+
+        public GetBookingsByCourtGroupIdEndpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("/api/court-groups/{CourtGroupId}/bookings")]
+        [SwaggerOperation(
+            Summary = "Get bookings by court group id",
+            Description = "Get bookings by court group id",
+            OperationId = "Bookings.GetByCourtGroupId",
+            Tags = new[] { "Bookings" }
+        )]
+        public override async Task<ActionResult> HandleAsync(GetBookingsByCourtGroupIdRequest request, CancellationToken cancellationToken = default)
+        {
+            var query = new GetBookingsByCourtGroupIdQuery
+            {
+                CourtGroupId = request.CourtGroupId,
+                BookingParameters = new BookingParameters
+                {
+                    PageNumber = request.PageNumber,
+                    PageSize = request.PageSize
+                }
+            };
+
+            Result<PagedList<BookingDto>> result = await _mediator.Send(query, cancellationToken);
+            
+            if (!result.IsSuccess)
+                return result.IsNotFound() ? NotFound(result) : BadRequest(result);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ PickleBall.Application/UseCases/UseCase_Booking/Queries/GetBookingsByCourtGroupId/GetBookingsByCourtGroupIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using Ardalis.Result;
+using MediatR;
+using PickleBall.Domain.DTOs.BookingDtos;
+using PickleBall.Domain.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickleBall.Application.UseCases.UseCase_Booking.Queries.GetBookingsByCourtGroupId
+{
+    public class GetBookingsByCourtGroupIdQuery : IRequest<Result<PagedList<BookingDto>>>
+    {
+        public Guid CourtGroupId { get; set; }
+        public bool TrackChanges { get; set; } = false;
+        public BookingParameters BookingParameters { get; set; } = new BookingParameters();
+    }
+}
