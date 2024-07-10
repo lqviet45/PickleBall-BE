b6a30741e4ff8516ce88df3069a99509758dfc5d
Long Bao <mglongbao@gmail.com>
2024-07-10 11:19:25 +0700

    add authorization

--- PickleBall.API/Program.cs
+++ PickleBall.API/Program.cs
@@ -46,7 +46,7 @@ builder
         };
     });
 
-
+builder.Services.AddAuthorization();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
