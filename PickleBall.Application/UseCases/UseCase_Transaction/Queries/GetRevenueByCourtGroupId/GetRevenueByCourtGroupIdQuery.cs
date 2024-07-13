789a1598c6f42262a8bcfbb37e2ee5aba0eef8ca
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2024-07-13 11:26:55 +0700

    add get revenue by courtgroup id

---
+++ PickleBall.Application/UseCases/UseCase_Transaction/Queries/GetRevenueByCourtGroupId/GetRevenueByCourtGroupIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using Ardalis.Result;
+using MediatR;
+using PickleBall.Domain.DTOs.TransactionDtos;
+
+namespace PickleBall.Application.UseCases.UseCase_Transaction.Queries.GetRevenueByCourtGroupId
+{
+    public class GetRevenueByCourtGroupIdQuery : IRequest<Result<RevenueResponseDto>>
+    {
+        public Guid CourtGroupId { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public bool TrackChanges { get; set; } = false;
+    }
+}
