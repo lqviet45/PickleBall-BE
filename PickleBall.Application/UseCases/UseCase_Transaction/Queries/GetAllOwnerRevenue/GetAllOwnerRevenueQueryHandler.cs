9b10e06011b850c39ea05529bca430983a846407
lqviet45 <lqviet455@gmail.com>
2024-10-15 10:19:05 +0700

    fix bugs

--- PickleBall.Application/UseCases/UseCase_Transaction/Queries/GetAllOwnerRevenue/GetAllOwnerRevenueQueryHandler.cs
+++ PickleBall.Application/UseCases/UseCase_Transaction/Queries/GetAllOwnerRevenue/GetAllOwnerRevenueQueryHandler.cs
@@ -46,7 +46,9 @@ public class GetAllOwnerRevenueQueryHandler : IRequestHandler<GetAllOwnerRevenue
             .AsTracking()
             .ToListAsync(cancellationToken: cancellationToken);
         
+        var toltal = revenue.Sum(t => t.Amount);
         var calendar = CultureInfo.InvariantCulture.Calendar;
+        
         var totalRevenueByWeek = revenue
             .GroupBy(t => GetWeekOfMonth(t.CreatedOnUtc.Date, calendar))
             .Select(t => new RevenueByAllOwnerDto()
@@ -74,9 +76,16 @@ public class GetAllOwnerRevenueQueryHandler : IRequestHandler<GetAllOwnerRevenue
     private static int GetWeekOfMonth(DateTime date, Calendar calendar)
     {
         var firstDayOfMonth = new DateTime(date.Year, date.Month, 1);
-        var firstWeekOfMonth = calendar.GetWeekOfYear(firstDayOfMonth, CalendarWeekRule.FirstDay, DayOfWeek.Monday);
-        var currentWeek = calendar.GetWeekOfYear(date, CalendarWeekRule.FirstDay, DayOfWeek.Monday);
 
-        return currentWeek - firstWeekOfMonth + 1;  // Subtract to get relative week of the month
+        // Find the first Monday of the month
+        var firstMonday = firstDayOfMonth;
+        while (firstMonday.DayOfWeek != DayOfWeek.Monday)
+        {
+            firstMonday = firstMonday.AddDays(1);
+        }
+
+        // Calculate the number of full weeks between the first Monday and the given date
+        var daysDifference = (date - firstMonday).Days;
+        return (daysDifference / 7) + 1;
     }
 }
